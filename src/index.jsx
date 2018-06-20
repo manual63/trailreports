404b125f355f66d97d342ee79cb0ac3aa1b84249
Shad Holland <shad.holland@bestbuy.com>
2018-06-20 10:26:34 -0500

    Added landing as default in layout

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 
 import Layout from './components/layout';
+import Landing from './components/landing';
 import Login from './components/login';
 
 const wrapper = document.getElementById('app');
 wrapper ? ReactDOM.render((
     <Router history={hashHistory}>
         <Route path="/" component={Layout}>
+            <IndexRoute component={Landing} />
             <Route path="/login" component={Login} />
         </Route>
     </Router>
