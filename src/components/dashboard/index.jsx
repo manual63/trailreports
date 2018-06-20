bec9133380caf537c4658f1cde133b67623cb626
Shad Holland <shad.holland@bestbuy.com>
2018-06-20 15:02:13 -0500

    Working on getting nav to update

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -3,20 +3,42 @@ import React from 'react';
 import styles from './styles.less';
 
 class Dashboard extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            activeNav: 'reports'
+        }
+    }
+
+    componentDidUpdate() {
+        console.log(this.props.location.pathname);
+        this.setActiveNav(this.props.location.pathname);
+    }
+
+    setActiveNav(pathname) {
+        switch(pathname) {
+            case '/reports':
+                this.setState({activeNav: 'reports'});
+                break;
+            case '/create':
+                this.setState({activeNav: 'create'});
+                break;
+        }
     }
 
     render() {
+        const activeNav = this.state.activeNav;
+
         return (
             <div>
                 <div className={styles.navWrapper}>
                     <ul className="nav nav-pills">
                         <li className="nav-item">
-                            <a className="nav-link active" href="#/reports">Reports</a>
+                            <a className={`nav-link ${activeNav === 'reports' ? 'active' : ''}`} href="#/reports">Reports</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#/create">Create</a>
+                            <a className={`nav-link ${activeNav === 'create' ? 'active' : ''}`} href="#/create">Create</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#/trailconditions">Trail Conditions</a>
