af4289700ae87323d99a75dba9b395962594825a
Shad Holland <shad.holland@bestbuy.com>
2018-06-20 10:13:09 -0500

    Login view basics

--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -3,8 +3,12 @@ import React from 'react';
 import styles from './styles.less';
 
 class Landing extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+    }
+
+    goToLogin() {
+        this.props.history.push('/login');
     }
 
     render() {
@@ -14,7 +18,7 @@ class Landing extends React.Component {
                     <p>Welccome to the Trail Reporting App!</p>
                 </div>
                 <div className={styles.signInWrapper}>
-                    <button>Sign In</button>
+                    <button onClick={this.goToLogin}>Sign In</button>
                 </div>
             </div>
         );
--- src/components/login/index.js
+++ src/components/login/index.js
@@ -9,6 +9,25 @@ class Login extends React.Component {
         return (
             <div>
                 <h2>Login</h2>
+                <div className="row">
+                    <div className="col-xs-3 text-right">
+                        Username:
+                    </div>
+                    <div className="col-xs-9">
+                        <input type="text" id="username" />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-3 text-right">
+                        Password:
+                    </div>
+                    <div className="col-xs-9">
+                        <input type="password" id="password" />
+                    </div>
+                </div>
+                <div>
+                    <button>Sign In</button>
+                </div>
             </div>
         );
     }
