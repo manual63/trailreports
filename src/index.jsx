aabf1b7d207a9f7464faa5741e753ed3de4765d9
Shad Holland <shad.holland@bestbuy.com>
2018-06-20 09:28:38 -0500

    Added routing

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router'
+
 import Layout from './components/layout';
+import Login from './components/login';
 
 const wrapper = document.getElementById('app');
-wrapper ? ReactDOM.render(<Layout />, wrapper) : false;
-
+wrapper ? ReactDOM.render((
+    <Router history={hashHistory}>
+        <Route path="/" component={Layout}>
+            <Route path="/login" component={Login} />
+        </Route>
+    </Router>
+), wrapper) : false;
